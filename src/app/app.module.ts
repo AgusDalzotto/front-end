b98bb9ffe709953688324ef77abf1d336f98d017
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-03-22 21:13:49 -0300

    adición importante al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
-import { SocialComponent } from './componentes/social/social.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
@@ -13,15 +12,15 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardsoftskillComponent } from './componentes/hardsoftskill/hardsoftskill.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { FooterComponent } from './componentes/footer/footer.component';
-import { LogoArgentinaComponent } from './componentes/logo-argentina/logo-argentina.component';
-import { LogComponent } from './componentes/log/log.component';
-import { LogoComponent } from './componentes/logo/logo.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SocialComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
@@ -29,9 +28,6 @@ import { LogoComponent } from './componentes/logo/logo.component';
     HardsoftskillComponent,
     ProyectosComponent,
     FooterComponent,
-    LogoArgentinaComponent,
-    LogComponent,
-    LogoComponent
   ],
   imports: [
     BrowserModule,
