bc0c6b2a6dfd83fcb83cfee6e08c73be692023e8
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 19:03:44 -0300

    experience

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Experiencia } from 'src/app/model/experiencia';
 import { SExperienciaService } from 'src/app/service/s-experiencia.service';
 import { TokenService } from 'src/app/service/token.service';
 
@@ -8,18 +9,26 @@ import { TokenService } from 'src/app/service/token.service';
   styleUrls: ['./experiencia.component.css']
 })
 
-export class ExperienciaComponent {
-  experiencia: ExperienciaComponent[] = [];
+export class ExperienciaComponent implements OnInit {
+  expe: Experiencia[] = [];
 
   constructor(private sExperiencia: SExperienciaService, private tokenService: TokenService){}
 
   isLogged = false;
 
   ngOnInit(): void {
+    this.cargarExperiencia();
     if (this.tokenService.getToken()){
       this.isLogged = true;
     } else {
       this.isLogged = false;
     }
   }
+
+  
+  cargarExperiencia(): void{
+    this.sExperiencia.lista().subscribe(data => {this.expe = data});
+  }
+  
 }
+
