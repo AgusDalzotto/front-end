597adc4e2e331b29527d579da3c1f428e156c712
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 18:58:10 -0300

    experience

--- src/app/componentes/experiencia/experiencia.component.ts
+++ src/app/componentes/experiencia/experiencia.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { SExperienciaService } from 'src/app/service/s-experiencia.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.css']
 })
-export class ExperienciaComponent {
 
+export class ExperienciaComponent {
+  experiencia: ExperienciaComponent[] = [];
+
+  constructor(private sExperiencia: SExperienciaService, private tokenService: TokenService){}
+
+  isLogged = false;
+
+  ngOnInit(): void {
+    if (this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
 }
