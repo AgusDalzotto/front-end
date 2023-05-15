80bf0c67f9326f846ff5f92122d1e58e9da6808e
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 09:30:09 -0300

    edu

--- src/app/model/educacion.ts
+++ src/app/model/educacion.ts
@@ -1,2 +1,11 @@
 export class Educacion {
+    id: number;
+    nombreE: string;
+    descripcionE: string;
+
+    constructor(nombreE: string, descripcionE: string) {
+        this.nombreE = nombreE
+        this.descripcionE = descripcionE
+    }
+
 }
