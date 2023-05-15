3bdbe15aeffc2a4f16ef74645f92780c9eea0571
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 09:44:03 -0300

    edu

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/service/educacion.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-educacion',
@@ -10,10 +12,27 @@ import { Educacion } from 'src/app/model/educacion';
 export class EducacionComponent implements OnInit {
 
   educacion: Educacion[] = [];
-  constructor() {}
+
+  constructor(private educacionS: EducacionService, private tokenService: TokenService) {}
+
+  isLogged= false;
 
   ngOnInit(): void {
-      
+    this.cargarEducacion();
+    if (this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
   }
 
+  cargarEducacion(): void {
+    this.educacionS.lista().subscribe(
+      data =>{
+        this.educacion = data;
+      }
+    )
+  }
+
+  
 }
