5868677856c6a6641691a3e17b5e38643ba4c447
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 09:34:38 -0300

    edu

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Educacion } from '../model/educacion';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class EducacionService {
-  URL = 'http://localhost:8080/educacion/'
-  constructor() { }
+  URL = 'http://localhost:8080/educacion/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public lista(): Observable<Educacion[]> {
+    return this.httpClient.get<Educacion[]>(this.URL + 'lista');
+  }
 }
