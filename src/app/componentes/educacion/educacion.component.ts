d91d1ceb1d7774ceb3d4accbdc430c0c7c165bec
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 09:40:41 -0300

    edu

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Educacion } from 'src/app/model/educacion';
 
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.css']
 })
-export class EducacionComponent {
+
+export class EducacionComponent implements OnInit {
+
+  educacion: Educacion[] = [];
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
 
 }
