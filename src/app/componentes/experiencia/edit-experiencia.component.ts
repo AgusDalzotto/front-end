581b5e8bc2453af1ac263304b26b846a3cc34316
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-14 21:08:54 -0300

    exp

--- src/app/componentes/experiencia/edit-experiencia.component.ts
+++ src/app/componentes/experiencia/edit-experiencia.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Experiencia } from 'src/app/model/experiencia';
+import { SExperienciaService } from 'src/app/service/s-experiencia.service';
 
 @Component({
   selector: 'app-edit-experiencia',
   templateUrl: './edit-experiencia.component.html',
   styleUrls: ['./edit-experiencia.component.css']
 })
-export class EditExperienciaComponent {
 
-}
+export class EditExperienciaComponent implements OnInit {
+  expLab: Experiencia = null;
+
+  constructor(private sExperiencia: SExperienciaService, private activatedRouter: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+  }
+
+  onUpdate(): void{
+    const id = this.activatedRouter.snapshot.params["id"];
+    this.sExperiencia.update(id, this.expLab).subscribe(data => {
+      this.router.navigate(['']);
+    }, err =>{
+      alert("Error al modificar la experiencia");
+      this.router.navigate(['']);
+    });
+  }
+  }
