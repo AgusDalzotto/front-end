c5ee4c68e16f74fcefea3d1a45cbc8fdb13f6e5d
Agustin Dalzotto <96092053+AgusDalzotto@users.noreply.github.com>
2023-05-15 21:50:05 -0300

    change

--- src/app/componentes/hys/hys.component.ts
+++ src/app/componentes/hys/hys.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Skill } from 'src/app/model/skill';
+import { SkillService } from 'src/app/service/skill.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-hys',
   templateUrl: './hys.component.html',
   styleUrls: ['./hys.component.css']
 })
-export class HysComponent {
 
-}
 export class HysComponent implements OnInit {
   skill: Skill[] = [];
 
